93411c6f9faf06ed01e7627b68994016624a4fda
maandrealira <a00227574@tec.mx>
2026-04-15 18:26:05 -0600

    inicio de pagina homa

--- src/app/(private)/home/page.tsx
+++ src/app/(private)/home/page.tsx
@@ -1,3 +1,24 @@
+import Image from "next/image";
+import NotificationBell from "../../../../public/notification_bell.png";
+
 export default function Home() {
-  return <div className="p-4">Home</div>;
+  return (
+    <section className="flex h-[100svh] w-full overflow-hidden bg-gray-200">
+      <div className="px-50 py-8 pt-12 rounded-full">
+        <h3 className="text-4xl font-semibold text-gray-600 tracking-tight leading-tight">Bienvenido, Croissant</h3>
+      </div>
+     <div className="flex justify-between items-center">
+        <Image
+          src={NotificationBell}
+          alt="Notification Bell"
+          width={50}
+          height={50}
+          className="w-[10svw] select-none"
+          priority
+        />
+      </div>
+    </section>
+  );
 }
+
+
